fbad8b500b7986cea79955f3bfe87bdff41d7a94
Tomek <tom.dziob@gmail.com>
2026-03-31 12:29:23 +0200

    Added basic add-on structure

--- 02-js/README.md
+++ 02-js/README.md
@@ -2,8 +2,15 @@
 
 ## Setup
 
-Windows:
+Docker:
 ```pwsh
 docker build -t minecraft-builder .
 docker run --rm -v "${env:APPDATA}\Minecraft Bedrock\Users\Shared\games\com.mojang\development_behavior_packs\CastleScript:/out" minecraft-builder
+```
+
+Windows:
+```pwsh
+npm install
+npm install --save-dev nodemon shx
+npm run watch
 ```
--- 02-js/scripts/main.js
+++ 02-js/scripts/main.js
@@ -7,7 +7,30 @@ world.afterEvents.playerSpawn.subscribe((event) => {
         const player = event.player;
 
         system.run(() => {
-            world.sendMessage(`[Castle Script] Hello World, ${player.name}!`);
+            world.sendMessage(`[Castle Script] Hello World, ${player.name}!
+                Type !castle to run the script.`);
+        });
+    }
+}); 
+
+world.afterEvents.chatSend.subscribe((event) => {
+    if (event.message === "!castle") {
+        const player = event.sender;
+
+        system.run(() => {
+            BuildCastle(player);
+            world.sendMessage(`[Castle Script] Starting build for ${player.name}...`);
         });
     }
 });
+
+function BuildCastle(player) {
+    // could be normal World, Nether or End
+    const dimension = player.dimension;
+
+    const player_x = Math.floor(player.location.x)
+    const player_y = Math.floor(player.location.y)
+    const player_z = Math.floor(player.location.z)
+
+    world.sendMessage(`Player stands at (${player_x}, ${player_y}, ${player_z})`)
+}
